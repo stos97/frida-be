93ca4a883137cf38409ee6abae03bb608222e07b
tos <stevann.tosic@gmail.com>
2024-10-15 18:03:35 +0200

    add auth

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+        Route::get('profile', [AuthController::class, 'profile'])->name('profile');
+    });
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
