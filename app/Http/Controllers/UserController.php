b0b7d7573c72db92b75edfb60bde5bbdf96a5148
tos <stevann.tosic@gmail.com>
2024-10-18 11:11:23 +0200

    add delete user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,4 +45,11 @@ class UserController extends Controller
 
         return new UserResource($user);
     }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->noContent();
+    }
 }
