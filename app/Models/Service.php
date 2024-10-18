48c51efa2d700b80e3ee87b23603d880e00fd85b
tos <stevann.tosic@gmail.com>
2024-10-18 12:57:44 +0200

    add service model

---
+++ app/Models/Service.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Service extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
